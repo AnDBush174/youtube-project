d9c3e1fd8b3f965f5bb31964624eba64de20729e
Andrew <andbushuev@yandex.ru>
2023-08-25 13:00:24 +0500

    Реализован  класс Channel в файле src/channel.py Реализован метод print_info() класса Channel Код в файле main.py выдаёт ожидаемое значение

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,33 @@
-
+import os
+from googleapiclient.discovery import build
 
 class Channel:
-    """Класс для ютуб-канала"""
+    def __init__(self, channel_id):
+        # Получение API-ключа из переменной окружения
+        api_key = os.getenv('YT_API_KEY')
+        # Переменная channel_id обозначена в конструкторе класса
+        self.channel_id = channel_id
+        # Создание объекта YouTube Data API с использованием api_key
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    def print_info(self):
+        # Выполнение запроса к YouTube Data API для получения информации о канале
+        channel_info = self.youtube.channels().list(
+            part='snippet',
+            id=self.channel_id
+        ).execute()
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        if channel_info['items']:
+            # Извлечение информации о канале из ответа
+            channel_snippet = channel_info['items'][0]['snippet']
+            channel_title = channel_snippet['title']
+            channel_description = channel_snippet['description']
+
+            # Вывод информации о канале
+            print("Channel information:")
+            print(f"Title: {channel_title}")
+            print(f"Description: {channel_description}")
+        else:
+            # Вывод сообщения об ошибке, если информацию о канале не удалось получить
+            print("Unable to fetch channel information.")
+

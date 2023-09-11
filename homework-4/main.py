d874e73e5f36047c5c48fb0e8b864e50c79f9f69
Andrew <andbushuev@yandex.ru>
2023-09-11 11:36:51 +0500

    Ожидаемое поведение кода в файле main.py: вывод ожидаемых значений.

--- homework-4/main.py
+++ homework-4/main.py
@@ -1,8 +1,20 @@
+# main.py
 from src.video import Video, PLVideo
 
 if __name__ == '__main__':
-    # Создаем два экземпляра класса
+    # Создаем два экземпляра класса с реальными данными
     video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
-    video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
-    assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+    video2 = PLVideo(
+        '4fObz_qw9u4',
+        'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC'
+    )
+    print(f"Название видео: {video1}")
+    print(f"Ссылка на видео: {video1.link}")
+    print(f"Количество просмотров: {video1.views}")
+    print(f"Количество лайков: {video1.likes}")
+    print()
+    print(f"Название видео: {video2}")
+    print(f"Ссылка на видео: {video2.link}")
+    print(f"Количество просмотров: {video2.views}")
+    print(f"Количество лайков: {video2.likes}")
+    print(f"ID плейлиста: {video2.playlist_id}")
